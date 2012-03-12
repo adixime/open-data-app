128f5c466642cda165cae62ebbefb68cca8f4c0f
adixime <adixime@gmail.com>
2012-03-12 14:00:00 -0400

    Added all the files for Public and Admin, and linked the index.php with the mordenizer.

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Community Gardens</title>
+    <link href="css/public.css" rel="stylesheet">
+    <script src="js/modernizr-2.0.6.js"></script>
+</head>
+
+<body>
+
+
+</body>
+</html>
