5049bbc65ffdd3bd8f4774ddb9bfd809e94b7039
adixime <adixime@gmail.com>
2012-03-17 10:47:07 -0400

    Added code to index.php and single.php to display the database to the user...

--- index.php
+++ index.php
@@ -1,17 +1,44 @@
 <?php 
 
+require_once 'includes/filter-wrapper.php';
+
+require_once 'includes/db.php';
+//var_dump($db);
+//->exec() allows us to perform SQ: and NOT expect results
+//->query() allows us to perform SQL and expect results
+$results = $db->query(' SELECT id, name, street_address, longitude, latitude 
+						FROM open_data_app
+					');
+//var_dump($db->errorInfo());
+//var_dump($results);
 
 ?><!DOCTYPE HTML>
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>Community Gardens</title>
-    <link href="css/public.css" rel="stylesheet">
-    <script src="js/modernizr-2.0.6.js"></script>
+	<title>Open Data App Prototype</title>
+	<link href="css/public.css" rel="stylesheet">
+	<link href="css/admin.css" rel="stylesheet">
 </head>
 
 <body>
 
+	<a href="add.php">Add a Community Garden!</a>
+    
+	<ul>
+		<?php /*foreach ($results as $movie) {
+                echo '<li>' . $movie['movie_title'] . '</li>';
+              }*/
+        ?>
+        
+        <?php foreach ($results as $garden) : ?>
+        	<li>
+            	<a href="single.php?id=<?php echo $garden['id']; ?>"><?php echo $garden['name']; ?></a>
+                <a href="edit.php?id=<?php echo $garden['id']; ?>">Edit</a>
+                <a href="delete.php?id=<?php echo $garden['id']; ?>">Delete</a>
+            </li>
+        <?php endforeach; ?>
+	</ul>
 
 </body>
 </html>
--- single.php
+++ single.php
@@ -1,1 +1,60 @@
 <?php
+
+require_once 'includes/filter-wrapper.php';
+
+require_once 'includes/db.php';
+
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if(empty($id)) {
+	header('Location: index.php');
+	exit; //Stop the PHP compiler right here and immediately redirect the user	
+}
+
+//Only connect to the database if there is an ID, because this is after the above if statement
+//Witout an ID there is no point connecting to the database
+
+//3rd way of getting info from database or to put in but it allows to change how the query works based on the info added.
+//->prepare() allows us to execute SQL with user input
+//this also prepares against sql injection
+$sql = $db->prepare('
+	SELECT id, name, street_address, longitude, latitude 
+	FROM open_data_app
+	where id = :id
+');
+//->bindValue() lets us fill in placeholders in our prepared statement
+// :id is a placeholder for us to SECURELY put information 
+$sql->bindValue(':id', $id, PDO::PARAM_INT);
+// Performs the SQL query on the database
+$sql->execute();
+// Gets the results from the sql query and stores them in a variable
+// fetchAll() if we expect to get all results
+// fetch() if we need to get one result
+$results = $sql->fetch();
+//Redirect the user back to the homepage if ther eare no database results
+//No results will happen if they chande the ?id=4 to include an ID that doesn't exist
+if(empty($results)) {
+	header('Location: index.php');
+	exit; //Stop the PHP compiler right here and immediately redirect the user	
+}
+
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Open Data App Prototype</title>
+	<link href="css/general.css" rel="stylesheet">
+	<link href="css/admin.css" rel="stylesheet">
+</head>
+
+<body>
+
+	<h1><?php echo $results['name']; ?></h1>
+    <p>Street Address: <?php echo $results['street_address']; ?></p>
+	<p>Longitude: <?php echo $results['longitude']; ?></p>
+	<p>Latitude: <?php echo $results['latitude']; ?></p>
+	<a href="index.php">Home</a>
+    
+</body>
+</html>
