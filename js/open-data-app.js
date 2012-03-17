d023724ee07e02ca5f8669d1729e7756944b9ccc
adixime <adixime@gmail.com>
2012-03-17 16:54:11 -0400

    madechanges to the js file...

--- js/open-data-app.js
+++ js/open-data-app.js
@@ -14,12 +14,12 @@ $(document).ready(function () {
 
 	// Loop through all the places and add a marker to the GMap
 	$('.gardens li').each(function (i, elem) {
-		var dino = $(this).find('a').html();
+		var garden = $(this).find('a').html();
 
 		// Create some HTML content for the info window
 		// Style the content in your CSS
 		var info = '<div class="info-window">'
-			+ '<strong>' + dino + '</strong>'
+			+ '<strong>' + garden + '</strong>'
 			+ '</div>'
 		;
 
@@ -28,16 +28,16 @@ $(document).ready(function () {
 		var lng = $(this).find('meta[itemprop="longitude"]').attr('content');
 		var pos = new google.maps.LatLng(lat, lng);
 
-		// Create a marker object for this dinosaur
+		// Create a marker object for this garden
 		var marker = new google.maps.Marker({
 			position : pos
 			, map : map
-			, title : dino
+			, title : garden
 			, icon : 'images/marker.png'
 			, animation: google.maps.Animation.DROP
 		});
 
-		// A function for showing this dinosaur's info window
+		// A function for showing this garden's info window
 		function showInfoWindow (ev) {
 			if (ev.preventDefault) {
 				ev.preventDefault();
