dde4f856a1ee62dd5860e2d905787eaacd0bd114
adixime <adixime@gmail.com>
2012-04-12 00:46:30 -0400

    minor changes...

--- includes/user-top.php
+++ includes/user-top.php
@@ -5,6 +5,7 @@
 	<title>Open Data App Prototype</title>
 	<link href="css/public.css" rel="stylesheet">
 	<script src="js/modernizr.dev.js"></script>
+	<link href='http://fonts.googleapis.com/css?family=Norican' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
