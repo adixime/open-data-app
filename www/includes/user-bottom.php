fad5beb90491cca7e141818148fa9fd3b1ba29f4
adixime <adixime@gmail.com>
2012-04-28 22:45:32 -0400

    Put create user in a safe location, added disqs comments

--- www/includes/user-bottom.php
+++ www/includes/user-bottom.php
@@ -4,6 +4,18 @@
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyD_NmRReBhRlMhSl9NA1ne7w2YFK3yCE68&sensor=false"></script>
 	<script src="js/open-data-app.js"></script>
     <script src="js/latlng.min.js"></script>
+    <script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+    var disqus_shortname = 'communitygardensapp'; // required: replace example with your forum shortname
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+</script>
 	
 </body>
 </html>
