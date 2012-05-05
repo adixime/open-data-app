862829d8c17d53c00ab1ef62d7dab6c9ceb091a4
adixime <adixime@gmail.com>
2012-05-05 12:29:45 -0400

    Shifted disqs from index page to single page.

--- www/index.php
+++ www/index.php
@@ -76,22 +76,6 @@ include 'includes/user-top.php';
 	<footer>
 		<p>copyright @adixime, 2012</p>
 	</footer>
-    
-    <div id="disqus_thread"></div>
-	<script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'communitygardensapp'; // required: replace example with your forum shortname
-    
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    <a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
-	
 
 <?php
 include 'includes/user-bottom.php';
